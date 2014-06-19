cdf1032d4aef37b545fc31d6247e43358a390b2d
Shabi <shabi@shabi-Inspiron-1525.(none)>
2014-06-19 09:58:27 +0530

    added basic logic for inverse not stable

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(inverse) m <<- inverse
+    
+    getinverse <- function() m
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      a <- makeCacheMatrix(x, ...)
+      
+      m <- a$getmean()
+      if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+      }
+      data <- a$get()
+      m <- solve(data)
+      print(m)
+      a$setmean(m)
+      m  
 }
